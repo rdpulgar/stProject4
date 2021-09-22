0b5449f0f081225e31ae1721157cb38f19ad0890
“rdpulgar” <66042231+rdpulgar@users.noreply.github.com>
2021-09-21 22:09:51 -0500

    u

--- spanish_sentiment_analysys.py
+++ spanish_sentiment_analysys.py
@@ -32,8 +32,8 @@ def main():
     uploaded_file = st.file_uploader("O bien puede seleccionar un archivo CSV para procesar múltiples párrafos (se procesará columna 'text')",type=['csv'])
     if uploaded_file is not None:
         if st.button("Procesar Archivo CSV"):
-            data = pd.read_csv(uploaded_file,nrows=3001)
-            #pd.read_parquet("penguin-dataset.parquet"
+            data = pd.read_csv(uploaded_file,usecols=["text"],nrows=3001)
+            #pd.read_parquet("penguin-dataset.parquet")
             #data.to_parquet("penguin-dataset.parquet")
             st.success("Procesando CSV ..")
             data[data['text'].str.strip().astype(bool)]
